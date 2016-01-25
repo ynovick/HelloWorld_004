294da3f43c0b37fc0e0326060edb7a59add60f48
Yitzchak Novick <ynovick@gmail.com>
2016-01-25 13:32:20 -0500

    added a generated message and displayed it to the screen

--- HelloWorld_004/src/mco152/MessageGenerator.java
+++ HelloWorld_004/src/mco152/MessageGenerator.java
@@ -1,5 +1,8 @@
 package mco152;
 
 public class MessageGenerator {
-
+	public static String getMessage()
+	{
+		return "Hello world!";
+	}
 }
--- HelloWorld_004/src/mco152/Program.java
+++ HelloWorld_004/src/mco152/Program.java
@@ -3,7 +3,6 @@ package mco152;
 public class Program {
 	public static void main(String[] args)
 	{
-		// YN was here
-		// YN was online also
+		System.out.println(MessageGenerator.getMessage());
 	}
 }
