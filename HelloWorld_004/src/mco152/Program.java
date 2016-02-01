f6cf7ade17e34234c8257db9ba17a509663a9c95
ynovick <ynovick@gmail.com>
2016-02-01 18:50:14 -0500

    Cleaned up message

--- HelloWorld_004/src/mco152/Program.java
+++ HelloWorld_004/src/mco152/Program.java
@@ -3,7 +3,7 @@ package mco152;
 public class Program {
 	public static void main(String[] args)
 	{
-		System.out.println("You are about to see a message"); // introductory message
+		System.out.println("You are about to see a message"); // message
 		System.out.println(MessageGenerator.getMessage());
 	}
 }
